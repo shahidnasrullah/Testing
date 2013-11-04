cf279938002c7c0cf0c9253e8f737bead27d4f03
Shahid Nasrullah <shahid.nasrullah@yahoo.com>
2013-11-04 18:43:04 +0500

    Xcode Project Added

---
+++ Testing1/Testing1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Testing1
+//
+//  Created by Adil Soomro on 04/11/2013.
+//  Copyright (c) 2013 Smonte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Testing1/Testing1Tests/Testing1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Testing1Tests.m
+//  Testing1Tests
+//
+//  Created by Adil Soomro on 04/11/2013.
+//  Copyright (c) 2013 Smonte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Testing1Tests : XCTestCase
+
+@end
+
+@implementation Testing1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
